33dd9b621cf543fe34061f5a410622a043aedd8c
Josh Souter-Brown <josh.gravicast@gmail.com>
2022-08-08 18:52:09 -0400

    added more subclasses to Car and Truck

--- exersise_challenge_2.js
+++ exersise_challenge_2.js
@@ -3,7 +3,7 @@
 // Car must have at least One unique property
 // Truck must have at least One unique property a& Must include the ability to Tow, with method to "activate 4 wheel drive".
 // Must have at least 2 instances of each Class.
-
+// Create 2 subclasses for each subclass. Car --> MiniCar/LargeCar || Truck --> Can Tow/Cannot Tow
 
 class Vehicle {
     constructor(motor, doors, broken) {
@@ -32,6 +32,21 @@ class Car extends Vehicle {
     }
 }
 
+class MiniCar extends Car {
+    constructor(motor, doors, broken, cargoAccess, roofType, origin) {
+        super(motor, doors, broken, cargoAccess, roofType);
+        this.origin = origin;
+    }
+}
+
+class LargeCar extends Car {
+    constructor(motor, doors, broken, cargoAccess, roofType, forFamily) {
+        super(motor, doors, broken, cargoAccess, roofType);
+        this.forFamily = forFamily;
+    }
+}
+
+
 class Truck extends Vehicle {
     constructor(motor, doors, broken, tow = false, fourWheelDrive = false) {
         super(motor, doors, broken);
@@ -45,15 +60,38 @@ class Truck extends Vehicle {
     }
 }
 
+class oversizedTow extends Truck {
+    constructor(motor, doors, broken, tow = true, fourWheelDrive, towBoat) {
+        super(motor, doors, broken, tow, fourWheelDrive);
+        this.towBoat = towBoat;
+    }
+}
 
-const blue = new Vehicle('130 hp', 4, false)
-const red = new Vehicle('160 hp', 2, false)
+class neverTow extends Truck {
+    constructor(motor, doors, broken, tow = true, fourWheelDrive, solidBumper) {
+        super(motor, doors, broken, tow, fourWheelDrive);
+        this.solidBumper = solidBumper;
+    }
+}
 
-const blueCar = new Car('140 hp', 4, false, "Trunk", "Sun Roof")
-const redCar = new Car('200 hp', 2, false, "Hatch", "Drop Top")
 
-const greenTruck = new Truck('350 hp', 2, false, true)
-const brokenTruck = new Truck('0 hp', 2, true)
+
+const blue = new Vehicle('130 hp', 4, false);
+const red = new Vehicle('160 hp', 2, false);
+
+const blueCar = new Car('140 hp', 4, false, "Trunk", "Sun Roof");
+const redCar = new Car('200 hp', 2, false, "Hatch", "Drop Top");
+
+const greenTruck = new Truck('350 hp', 2, false, true);
+const brokenTruck = new Truck('0 hp', 2, true);
+
+const mini = new MiniCar("100 hp", 2, false, "hatch", "hard top", "Europe");
+const large = new LargeCar("180 hp", 8, false, "hatch", "moon roof", "Family");
+
+const bigtow = new oversizedTow("550 hp", 5, false, true, true, true)
+const useless = new neverTow("20 hp", 4, false, true, true, true)
+
+
 
 console.log(Vehicle.canDriveOnRoad(red))
 console.log(Vehicle.canDriveOnRoad(redCar))
@@ -62,3 +100,10 @@ console.log(Vehicle.canDriveOnRoad(brokenTruck))
 
 console.log(greenTruck.toggleFourWheelDrive())
 console.log(greenTruck.toggleFourWheelDrive())
+console.log(greenTruck.toggleFourWheelDrive())
+console.log(greenTruck.toggleFourWheelDrive())
+
+console.log(mini)
+console.log(large)
+console.log(bigtow)
+console.log(useless)
